eece50f4066622802ad6015e5760d526f4ea4b05
AngheloCarrillo <anghelo.carrillo@hotmail.com>
2026-02-12 21:47:14 -0600

    Eliminar todos los archivos excepto Proyecto modulo 1 python.py

--- class 1.py
+++
@@ -1,7 +0,0 @@
-print ("hola mundo")
-print ("Esta es mi primer clase de Fundamentos de python")
-print ("Mi nombre es Anghelo Carrillo")
-print ("Esta es la segunda clase de fundamentos de python")
-
-#Variables
-#operaciones
